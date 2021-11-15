a4d800cc97d1fac0415d3815e0bf0af6f1012885
redbor <redbor24@gmail.com>
2021-11-15 17:23:34 +0300

    - Количество вакансий только для Москвы; - Количество вакансий только для Москвы за всё время и за последний месяц

--- headhuter.py
+++ headhuter.py
@@ -2,7 +2,6 @@ import datetime
 import json
 import requests
 
-
 HH_BASE_URL = 'https://api.hh.ru/'
 
 HEADERS = {
@@ -16,9 +15,9 @@ params = {
 
 def programmer_vacancies():
     response = requests.get(
-            HH_BASE_URL + r'vacancies/',
-            headers=HEADERS,
-            params=params)
+        HH_BASE_URL + r'vacancies/',
+        headers=HEADERS,
+        params=params)
     response.raise_for_status()
     return response.json()
 
@@ -26,3 +25,21 @@ def programmer_vacancies():
 if __name__ == '__main__':
     print(f'Общий список вакансий "{params["text"]}":')
     print(json.dumps(programmer_vacancies(), indent=4, ensure_ascii=False))
+
+    # Список вакансий "программист" для Москвы
+    params['area'] = '1'
+    hh_response = programmer_vacancies()
+    print(json.dumps(hh_response, indent=4, ensure_ascii=False))
+
+    # Общее количество вакансий
+    print(f'Общее количество вакансий "{params["text"]}" для Москвы: '
+          f'{hh_response["found"]}')
+
+    # Список вакансий "программист" для Москвы за последний месяц
+    today = datetime.datetime.today()
+    params['date_from'] = (today + datetime.timedelta(days=-30)).\
+        strftime('%Y-%m-%d')
+    params['date_to'] = today.strftime('%Y-%m-%d')
+    print(f'Количество вакансий "{params["text"]}'
+          f'" для Москвы за последний месяц: '
+          f'{programmer_vacancies()["found"]}')
