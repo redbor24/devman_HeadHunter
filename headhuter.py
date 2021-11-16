99093268457db4dc83632b1e5dce3e8c7e62cd3b
redbor <redbor24@gmail.com>
2021-11-16 10:19:13 +0300

    Добавлена функция get_proglang_distribution() возвращает словарь языков программирования с количеством вакансий по каждому

--- headhuter.py
+++ headhuter.py
@@ -1,5 +1,6 @@
 import datetime
 import json
+import operator
 import requests
 
 HH_BASE_URL = 'https://api.hh.ru/'
@@ -8,12 +9,38 @@ HEADERS = {
     'content-type': 'application/json; charset=UTF-8',
 }
 
-params = {
+hh_params = {
     'text': 'программист',
 }
 
+lang_list = {
+    'Python': 0,
+    'Java': 0,
+    'JavaScript': 0,
+    'C++': 0,
+    'C#': 0,
+    'Delphi': 0,
+    'GO': 0,
+    'PHP': 0,
+    'Ruby': 0,
+}
 
-def programmer_vacancies():
+
+def get_proglang_distribution():
+    params = {}
+    for lang_n, lang in enumerate(lang_list):
+        params['text'] = 'программист ' + lang
+        response = requests.get(HH_BASE_URL + r'vacancies/', headers=HEADERS, params=params)
+        response.raise_for_status()
+        _ = response.json()['found']
+        lang_list[lang] = _ if _ > 100 else 0
+
+    return {k: v for k, v in sorted(lang_list.items(),
+                                    key=operator.itemgetter(1),
+                                    reverse=True)}
+
+
+def programmer_vacancies(params):
     response = requests.get(
         HH_BASE_URL + r'vacancies/',
         headers=HEADERS,
@@ -23,23 +50,25 @@ def programmer_vacancies():
 
 
 if __name__ == '__main__':
-    print(f'Общий список вакансий "{params["text"]}":')
-    print(json.dumps(programmer_vacancies(), indent=4, ensure_ascii=False))
+    print(f'Общий список вакансий "{hh_params["text"]}":')
+    print(json.dumps(programmer_vacancies(hh_params), indent=4, ensure_ascii=False))
 
     # Список вакансий "программист" для Москвы
-    params['area'] = '1'
-    hh_response = programmer_vacancies()
+    hh_params['area'] = '1'
+    hh_response = programmer_vacancies(hh_params)
     print(json.dumps(hh_response, indent=4, ensure_ascii=False))
 
     # Общее количество вакансий
-    print(f'Общее количество вакансий "{params["text"]}" для Москвы: '
+    print(f'Общее количество вакансий "{hh_params["text"]}" для Москвы: '
           f'{hh_response["found"]}')
 
     # Список вакансий "программист" для Москвы за последний месяц
     today = datetime.datetime.today()
-    params['date_from'] = (today + datetime.timedelta(days=-30)).\
+    hh_params['date_from'] = (today + datetime.timedelta(days=-30)).\
         strftime('%Y-%m-%d')
-    params['date_to'] = today.strftime('%Y-%m-%d')
-    print(f'Количество вакансий "{params["text"]}'
+    hh_params['date_to'] = today.strftime('%Y-%m-%d')
+    print(f'Количество вакансий "{hh_params["text"]}'
           f'" для Москвы за последний месяц: '
-          f'{programmer_vacancies()["found"]}')
+          f'{programmer_vacancies(hh_params)["found"]}')
+
+    print(f'Вакансии по ЯП: {get_proglang_distribution()}')
