bfcc1bd612889d1aeaa6ac577a8bb24b0af5dacb
redbor <redbor24@gmail.com>
2021-11-30 16:05:59 +0300

    Дополнено описание README.md Модуль headhunter.py переименован в pl_stat.py В config.py вынесен список языков программирования, по которым будет получена статистика вакансий

--- config.py
+++ config.py
@@ -6,11 +6,20 @@ HH_HEADER = {
     'content-type': 'application/json; charset=UTF-8',
 }
 
+SJ_BASE_URL = 'https://api.superjob.ru/2.0/'
 SJ_SECRET_KEY = config('SJ_SECRET_KEY', '')
 SJ_HEADER = {
     'X-Api-App-Id': SJ_SECRET_KEY,
 }
-SJ_BASE_URL = 'https://api.superjob.ru/2.0/'
 
-VAC_PER_PAGE = 100
-
+PROG_LANGS = [
+    'Python',
+    'Java',
+    'JavaScript',
+    'C++',
+    'C',
+    'Delphi',
+    'GO',
+    'PHP',
+    'Ruby',
+]
--- pl_stat.py
+++ pl_stat.py
@@ -4,24 +4,26 @@ import requests
 from requests import exceptions
 from terminaltables import SingleTable
 
-from config import HH_BASE_URL, HH_HEADER, SJ_BASE_URL, SJ_HEADER, VAC_PER_PAGE
+from config import (
+    HH_BASE_URL,
+    HH_HEADER,
+    SJ_BASE_URL,
+    SJ_HEADER,
+    PROG_LANGS
+)
+
+resources = ['HeadHunter.ru', 'SuperJob.ru']
+
+col_aligns = {
+    1: 'center',
+    2: 'center',
+    3: 'center',
+}
 
 lang_details = {
     'vacancies_found': 0,
     'vacancies_processed': 0,
     'average_salary': 0,
-}
-
-proglangs = {
-    'Python': 0,
-    'Java': 0,
-    'JavaScript': 0,
-    'C++': 0,
-    'C#': 0,
-    'Delphi': 0,
-    'GO': 0,
-    'PHP': 0,
-    'Ruby': 0,
 }
 
 
@@ -58,10 +60,10 @@ def predict_salary(salary_from, salary_to):
 
 
 def get_proglang_stat_hh(proglang, proglang_stat):
-    print(f'Подсчёт количества вакансий для "{proglang}"', end='')
+    print(f'Подсчёт количества вакансий для "{proglang}"...')
     params = {
         'text': proglang,
-        'per_page': VAC_PER_PAGE,
+        'per_page': 100,
         'page': '0',
     }
     vac_total, vacs_processed, salary_sum, average_salary, page = 0, 0, 0, 0, 0
@@ -71,6 +73,7 @@ def get_proglang_stat_hh(proglang, proglang_stat):
         vacs_processed += proglang_stat_values['vacancies_processed']
         average_salary += proglang_stat_values['average_salary']
         salary_sum = average_salary * vacs_processed
+
     while True:
         try:
             response = requests.get(
@@ -81,6 +84,7 @@ def get_proglang_stat_hh(proglang, proglang_stat):
             vacancies = response.json()
         except exceptions.HTTPError:
             break
+
         for vac_n, vac in enumerate(vacancies['items']):
             if vac['salary']:
                 if vac['salary']['currency'] == 'RUR':
@@ -90,9 +94,7 @@ def get_proglang_stat_hh(proglang, proglang_stat):
                         vacs_processed += 1
         page += 1
         params['page'] = page
-        print('.', end='')
 
-    print('')
     proglang_stat_values = {
         'vacancies_found': vacancies['found'],
         'vacancies_processed': vacs_processed,
@@ -104,13 +106,13 @@ def get_proglang_stat_hh(proglang, proglang_stat):
 
 
 def get_proglang_stat_sj(proglang, proglang_stat):
-    print(f'Подсчёт количества вакансий для "{proglang}"', end='')
+    print(f'Подсчёт количества вакансий для "{proglang}"...')
     params = {
         'town': 'Москва',
         'catalogues': '33',
         'keyword': proglang,
         'page': '0',
-        'count': VAC_PER_PAGE
+        'count': 100
     }
     response = requests.get(
         url=SJ_BASE_URL + 'vacancies/',
@@ -118,8 +120,8 @@ def get_proglang_stat_sj(proglang, proglang_stat):
         params=params
     )
     response.raise_for_status()
-    vac_on_page = 20
     vac_total = response.json()['total']
+    vac_on_page = 20
     pages = math.ceil(vac_total / vac_on_page)
     vacs_processed, salary_sum, average_salary, page = 0, 0, 0, 0
     if proglang in proglang_stat:
@@ -145,16 +147,32 @@ def get_proglang_stat_sj(proglang, proglang_stat):
                     salary_sum += predicted_salary
                     vacs_processed += 1
         params['page'] = page
-        print('.', end='')
 
-    print('')
     proglang_stat_values = {
         'vacancies_found': vac_total,
         'vacancies_processed': vacs_processed,
         'average_salary': int(salary_sum / vacs_processed)
     }
     proglang_stat[proglang] = proglang_stat_values
-    return proglang_stat_values
+    return proglang_stat
+
+
+def get_stat(resource):
+    if resource not in resources:
+        return None
+
+    langs = {}
+    for lang_n, lang in enumerate(PROG_LANGS):
+        langs[lang] = 0
+
+    stat = {}
+    print(resource)
+    for lang_n, lang in enumerate(langs):
+        if resource == 'HeadHunter.ru':
+            stat = get_proglang_stat_hh(lang, stat)
+        if resource == 'SuperJob.ru':
+            stat = get_proglang_stat_sj(lang, stat)
+    return stat
 
 
 def convert_data_for_tables(data_array):
@@ -174,8 +192,10 @@ def convert_data_for_tables(data_array):
     return result
 
 
-def print_table(stat, caption, column_aligns):
-    table_instance = SingleTable(convert_data_for_tables(stat), caption)
+def print_table(stat, table_caption, column_aligns):
+    if not stat:
+        return
+    table_instance = SingleTable(convert_data_for_tables(stat), table_caption)
     for item in column_aligns.items():
         table_instance.justify_columns[item[0]] = item[1]
     print(table_instance.table)
@@ -183,21 +203,8 @@ def print_table(stat, caption, column_aligns):
 
 
 if __name__ == '__main__':
-    col_aligns = {
-        1: 'center',
-        2: 'center',
-        3: 'center',
-    }
+    hh_stat = get_stat('HeadHunter.ru')
+    sj_stat = get_stat('SuperJob.ru')
 
-    hh_stat = {}
-    print('HeadHunter.ru')
-    for lang_n, lang in enumerate(proglangs):
-        get_proglang_stat_hh(lang, hh_stat)
     print_table(hh_stat, 'HeadHunter. Москва', col_aligns)
-
-    sj_stat = {}
-    print('SuperJob.ru')
-    for lang_n, lang in enumerate(proglangs):
-        get_proglang_stat_sj(lang, sj_stat)
-
     print_table(sj_stat, 'SuperJob. Москва', col_aligns)
